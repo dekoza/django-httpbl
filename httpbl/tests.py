ba6af27ee94a20d70e31173e29ff01880faef4d8
Dominik Kozaczko <dominik@kozaczko.info>
2012-06-16 11:01:04 +0200

    basic tests, not all cases covered

---
+++ httpbl/tests.py
@@ -0,0 +1,66 @@
+#coding: utf-8
+from django.utils import unittest
+from django.test.client import RequestFactory
+from httpbl.middleware import HttpBLMiddleware
+from httpbl.models import HttpBLLog
+
+
+class HttpBLMiddlewareTestCase(unittest.TestCase):
+	def setUp(self):
+		self.mw = HttpBLMiddleware()
+		self.mw.age = 1
+		self.mw.threat = 1
+		self.mw.api_key = 'abcdefghijkl' # API key for testing purposes. You should probably use your own.
+		self.logging = False
+		self.quicklink = 'http://google.com/'
+		self.req = RequestFactory()
+
+	def test_threat(self):
+		"""
+		This should redirect to QuickLink.
+		"""
+		self.mw.classification = 1
+		req = self.req.get('/')
+		req.environ['REMOTE_ADDR'] = '127.1.1.1'
+		threat = self.mw.is_threat(req)
+		self.assertTrue(threat)
+
+
+	def test_search_engine(self):
+		"""
+		This should allow the client in.
+		"""
+		self.mw.classification = 1
+		req = self.req.get('/')
+		req.environ['REMOTE_ADDR'] = '127.1.1.0'
+		threat = self.mw.is_threat(req)
+		self.assertFalse(threat)
+		response = self.mw.process_request(req)
+		self.assertEqual(response, None)
+
+
+	def test_innocent(self):
+		"""
+		This should let the client in.
+		"""
+		self.mw.classification = 1
+		req = self.req.get('/')
+		req.environ['REMOTE_ADDR'] = '127.0.0.1'
+		threat = self.mw.is_threat(req)
+		self.assertFalse(threat)
+		response = self.mw.process_request(req)
+		self.assertEqual(response, None)
+
+
+	def test_suspicious(self):
+		"""
+		This should let the client in setting 'httpbl_suspicious' context variable to True.
+		"""
+		self.mw.classification = 2
+		req = self.req.get('/')
+		req.environ['REMOTE_ADDR'] = '127.1.1.1'
+		threat = self.mw.is_threat(req)
+		self.assertFalse(threat)
+		response = self.mw.process_request(req)
+		self.assertEqual(response, None)
+
