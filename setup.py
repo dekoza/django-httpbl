50f2b19c813bc2c855e7a0bb3dc6755577128f63
Dominik Kozaczko <dominik@logrus>
2012-07-27 07:55:04 +0200

    there was error with version number in setup.py - now corrected ;) added skeleton for what will be an app for testing purposes

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2
 from distutils.core import setup
 
 with open('README.md') as file:
@@ -6,11 +7,12 @@ with open('README.md') as file:
 setup(
     name='django-httpbl',
     long_description=long_description,
-    version='0.1.20',
+    version='0.1.1',
     packages=['httpbl'],
     url='https://github.com/dekoza/django-httpbl',
     license='BSD',
     author='Dominik Kozaczko',
     author_email='dominik@kozaczko.info',
     description='Django middleware implementing Http:BL API of projecthoneypot.org'
+
 )
